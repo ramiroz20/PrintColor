6d10fa841dc1359a6dfd07dfea9b77b7adc6534d
Roman <romantuser69@gmail.com>
2022-02-05 23:42:34 +0500

    CheckSumm right

--- src/main/java/ru/CheckSum/com/CheckSum.java
+++ src/main/java/ru/CheckSum/com/CheckSum.java
@@ -2,22 +2,26 @@ package ru.CheckSum.com;
 
 
     public class CheckSum {
-        public static void main(String[] args) {
-            checkSumSign();
 
+        public static void checkSum(int a, int b) {
 
-        }
-
-        private static void checkSumSign() {
-
-            int a = 11, b = -12;
             if (a + b >= 0) {
                 System.out.println("сумма положительная");
             } else {
                 System.out.println("сумма отрицательная");
 
-
             }
         }
+
+        public static void main(String[] args) {
+
+            int a = -5;
+            int b = 55;
+            checkSum(a,b);
+
+        }
     }
+
+
+
 
